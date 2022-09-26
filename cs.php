6dbc737fa1a64e46c874cdb2a806ee16731194fb
rajanyajualan <98626950+rajanyajualan@users.noreply.github.com>
2022-09-26 13:21:41 +0700

    Add files via upload

---
+++ cs.php
@@ -0,0 +1,8 @@
+<script>
+<button onclick="var jsVar = "<?php 
+$nomor = array("6283800900888","6283136812307"); 
+$nomor1 = array("6283136812307"); 
+$nohp = $nomor[array_rand($nomor)]; 
+$text = "Hi kak,saya mau beli *Followers, Liker* Atau mau tanya tanya dulu !";
+header("Location: https://api.whatsapp.com/send?phone=$nohp&text=$text"); ?>"">
+</script>
